023b87edda599987b487022b516deecabf5b775f
Jonathan S <gereeter@gmail.com>
2013-09-04 18:58:41 -0500

    Rewrote implementation to use default values and one simple function that does all the work

--- src/Data/TLRef.hs
+++ src/Data/TLRef.hs
@@ -1,32 +1,41 @@
-module Data.TLRef (TLRef, newTLRef, writeTLRef, readTLRef) where
+module Data.TLRef (TLRef, newTLRef, readTLRef, writeTLRef, modifyTLRef) where
 
 import qualified Data.Map as M
 import Data.IORef
 import Control.Concurrent
 import Control.Applicative
 
--- | A variable that stores at most one value per thread
-newtype TLRef a = TLRef (IORef (M.Map ThreadId (IORef a)))
+-- | A variable that stores at one value per thread
+data TLRef a = TLRef a (IORef (M.Map ThreadId (IORef a)))
 
 -- | Create a new thread local reference
-newTLRef :: IO (TLRef a)
-newTLRef = TLRef <$> newIORef M.empty
+newTLRef :: a -> IO (TLRef a)
+newTLRef def = TLRef def <$> newIORef M.empty
 
--- | Write a value to the current thread's variable
-writeTLRef :: TLRef a -> a -> IO ()
-writeTLRef (TLRef mapRef) val = do
+getIORef :: TLRef a -> IO (IORef a)
+getIORef (TLRef def mapRef) = do
     tid <- myThreadId
     tlmap <- readIORef mapRef
     case M.lookup tid tlmap of
         Nothing -> do
-            ref <- newIORef val
+            ref <- newIORef def
             atomicModifyIORef' mapRef (\tlmap' -> (M.insert tid ref tlmap', ()))
-        Just ref -> writeIORef ref val
+            return ref
+        Just ref -> return ref
+
+-- | Write a value to the current thread's variable
+writeTLRef :: TLRef a -> a -> IO ()
+writeTLRef tlref val = do
+    ioref <- getIORef tlref
+    writeIORef ioref val
 
 -- | Read the current thread's value in a variable
-readTLRef :: TLRef a -> IO (Maybe a)
-readTLRef (TLRef mapRef) = do
-    mref <- M.lookup <$> myThreadId <*> readIORef mapRef
-    case mref of
-        Nothing -> return Nothing
-        Just ref -> Just <$> readIORef ref
+readTLRef :: TLRef a -> IO a
+readTLRef tlref = do
+    ioref <- getIORef tlref
+    readIORef ioref
+
+modifyTLRef :: TLRef a -> (a -> a) -> IO ()
+modifyTLRef tlref f = do
+    ioref <- getIORef tlref
+    modifyIORef ioref f
